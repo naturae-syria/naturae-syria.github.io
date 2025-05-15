8e6c5f8eed21d1026b4b9e52783130c893f993a9
AcriSign <amjadalmouayad@gmail.com>
2025-05-15 13:28:12 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +10,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // This ensures assets work correctly on GitHub Pages
+  assetPrefix: '.',
 }
 
 export default nextConfig
