0d82c4272fd5dcc27a3de1812238763e55de6a74
AcriSign <amjadalmouayad@gmail.com>
2025-05-15 14:19:37 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,10 @@ const nextConfig = {
     unoptimized: true,
   },
   // تأكد من تغيير هذا ليتطابق مع اسم المستودع الخاص بك
+  // لا تضع شرطة مائلة في البداية
   basePath: '/arabic-product-catalog',
   assetPrefix: '/arabic-product-catalog',
+  trailingSlash: true,
 }
 
 export default nextConfig
