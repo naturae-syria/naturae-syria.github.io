f5d3eaf61d40e85751fd5cc23f993ec5e3a639b1
naturae-syria <amjadalmouayad@gmail.com>
2025-06-04 16:41:40 -0300

    Add HTTPS server option

---
+++ server-https.js
@@ -0,0 +1,23 @@
+const { createServer } = require('https');
+const { readFileSync } = require('fs');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const PORT = process.env.PORT || 3000;
+const CERT_DIR = process.env.CERT_DIR || 'cert';
+
+app.prepare().then(() => {
+  const options = {
+    key: readFileSync(`${CERT_DIR}/privkey.pem`),
+    cert: readFileSync(`${CERT_DIR}/fullchain.pem`),
+  };
+
+  createServer(options, (req, res) => {
+    handle(req, res);
+  }).listen(PORT, () => {
+    console.log(`> Ready on https://localhost:${PORT}`);
+  });
+});
